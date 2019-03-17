fbdbe7cb3eeddf5efe7c65ace64c8d8949d0c1d3
Mark Skinner <markskinner850@gmail.com>
2019-03-17 16:13:06 -0400

    Add projects to html using javascript/jquery

--- js/main.js
+++ js/main.js
@@ -1,11 +1,43 @@
+class Project {
+    constructor(name, link, image) {
+        this.name = name;
+        this.link = link;
+        this.image = image;
+    }
+}
+
+const projects = [
+    new Project("Weather App", "#", "https://www.fillmurray.com/640/360"),
+    new Project("Drum Machine", "#", "https://www.placecage.com/640/360"),
+    new Project("Random Quote Generator", "#", "https://www.fillmurray.com/640/360"),
+    new Project("Calculator", "#", "https://www.placecage.com/640/360"),
+    new Project("Pomodoro Clock", "#", "https://www.fillmurray.com/640/360"),
+    new Project("Wikipedia Searcher", "#", "https://www.placecage.com/640/360"),
+    new Project("Markdown Previewer", "#", "https://www.fillmurray.com/640/360"),
+    new Project("Product Landing Page", "#", "https://www.placecage.com/640/360"),
+    new Project("Technical Documentation Page", "#", "https://www.fillmurray.com/640/360"),
+    new Project("Tic Tac Toe", "#", "https://www.placecage.com/640/360"),
+    new Project("Frogger", "#", "https://www.fillmurray.com/640/360")
+];
+
+function addProjects(arr) {
+    arr.forEach(item => {
+        $('#portfolio').append("<div class='project'><a href='" + item.link
+            + "' target='_blank'><img src='" + item.image
+            + "' alt='" + item.name + "'></a><h3>" +item.name + "</h3></div>");
+    });
+}
+
 $(function() {
+    addProjects(projects);
+
     $(".nav-link").on('click', function() {
         $("#nav-bar").removeClass("responsive");
         $("#container").css("width", "100%");
         $(".line").addClass("hidden");
         $(".fas").removeClass("fa-times");
         $(".fas").addClass("fa-bars");
-    })
+    });
 })
 
 function toggleMenu() {
