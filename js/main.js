05c020c2edcf487df1cf469321910964869eb634
Mark Skinner <markskinner850@gmail.com>
2019-03-20 14:22:09 -0400

    Change container width from 100vw to 100%

--- js/main.js
+++ js/main.js
@@ -67,7 +67,7 @@ $(function() {
 
     $(".nav-link").on('click', function() {
         $("#nav-bar").removeClass("responsive");
-        $("#container").css("width", "100vw");
+        $("#container").css("width", "100%");
         $(".line").addClass("hidden");
         $(".fas").removeClass("fa-times");
         $(".fas").addClass("fa-bars");
@@ -77,12 +77,12 @@ $(function() {
 function toggleMenu() {
     $("#nav-bar").toggleClass("responsive");
     if ($("#nav-bar").hasClass("responsive")) {
-        $("#container").css("width", "50vw");
+        $("#container").css("width", "50%");
         $(".line").removeClass("hidden");
         $(".fas").removeClass("fa-bars");
         $(".fas").addClass("fa-times");
     } else {
-        $("#container").css("width", "100vw");
+        $("#container").css("width", "100%");
         $(".line").addClass("hidden");
         $(".fas").removeClass("fa-times");
         $(".fas").addClass("fa-bars");
@@ -93,7 +93,7 @@ $(window).resize(function () {
     var viewportWidth = $(window).width();
     if (viewportWidth > 786) {
         $("#nav-bar").removeClass("responsive");
-        $("#container").css("width", "100vw");
+        $("#container").css("width", "100%");
         $(".line").addClass("hidden");
         $(".fas").removeClass("fa-times");
         $(".fas").addClass("fa-bars");
