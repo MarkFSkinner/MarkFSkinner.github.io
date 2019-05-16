8ef90563e1295adf3f8f904f8b7c0953c8a8a8e6
Mark Skinner <markskinner850@gmail.com>
2019-05-16 15:21:15 -0400

    Clean up code

--- js/main.js
+++ js/main.js
@@ -8,36 +8,36 @@ class Project {
 }
 
 const projects = [
-    new Project("Weather App", "https://weather-app-39e37.firebaseapp.com/", "images/weather-app.png", ["apps", "react"]),
-    new Project("Drum Machine", "https://drum-machine-5bfd0.firebaseapp.com/", "images/drum-machine.png", ["apps", "react"]),
-    new Project("Frogger", "https://markfskinner.github.io/costanza-frogger-game/", "images/frogger.png", ["apps"]),
-    new Project("Tic Tac Toe", "https://tic-tac-toe-cd139.firebaseapp.com/", "images/tic-tac-toe.png", ["apps", "react"]),
-    new Project("Calculator", "https://calculator-2aef9.web.app/", "images/calculator.png", ["apps", "react"]),
-    new Project("Pomodoro Clock", "https://pomodoro-clock-4efc4.firebaseapp.com/", "images/pomodoro-clock.png", ["apps", "react"]),
-    new Project("Markdown Previewer", "https://markdown-previewer-23554.firebaseapp.com/", "images/markdown-previewer.png", ["apps", "react"]),
-    new Project("Random Quote Machine", "https://markfskinner.github.io/random-quote-machine/", "images/random-quote-machine.png", ["apps"]),
-    new Project("Wikipedia Viewer", "https://markfskinner.github.io/wikipedia-viewer/", "images/wikipedia-viewer.png", ["apps"]),
-    new Project("Product Landing Page", "https://markfskinner.github.io/product-landing-page/", "images/product-landing-page.png", ["sites"]),
-    new Project("Technical Documentation Page", "https://markfskinner.github.io/technical-documentation-page/", "images/technical-documentation-page.png", ["sites"])
+    new Project('Weather App', 'https://weather-app-39e37.firebaseapp.com/', 'images/weather-app.png', ['apps', 'react']),
+    new Project('Drum Machine', 'https://drum-machine-5bfd0.firebaseapp.com/', 'images/drum-machine.png', ['apps', 'react']),
+    new Project('Frogger', 'https://markfskinner.github.io/costanza-frogger-game/', 'images/frogger.png', ['apps']),
+    new Project('Tic Tac Toe', 'https://tic-tac-toe-cd139.firebaseapp.com/', 'images/tic-tac-toe.png', ['apps', 'react']),
+    new Project('Calculator', 'https://calculator-2aef9.web.app/', 'images/calculator.png', ['apps', 'react']),
+    new Project('Pomodoro Clock', 'https://pomodoro-clock-4efc4.firebaseapp.com/', 'images/pomodoro-clock.png', ['apps', 'react']),
+    new Project('Markdown Previewer', 'https://markdown-previewer-23554.firebaseapp.com/', 'images/markdown-previewer.png', ['apps', 'react']),
+    new Project('Random Quote Machine', 'https://markfskinner.github.io/random-quote-machine/', 'images/random-quote-machine.png', ['apps']),
+    new Project('Wikipedia Viewer', 'https://markfskinner.github.io/wikipedia-viewer/', 'images/wikipedia-viewer.png', ['apps']),
+    new Project('Product Landing Page', 'https://markfskinner.github.io/product-landing-page/', 'images/product-landing-page.png', ['sites']),
+    new Project('Technical Documentation Page', 'https://markfskinner.github.io/technical-documentation-page/', 'images/technical-documentation-page.png', ['sites'])
 ];
 
 function addProjects(arr) {
     arr.forEach(item => {
-        let classes = item.categories.join(" ");
-        $("#portfolio").append("<div class='project-container " + classes + "'><a href='" + item.link
-            + "' target='_blank'><div class='project'><div class='img-container'><img src='" + item.image
-            + "' alt='" + item.name + "'></div><h3>" + item.name + "</h3></div></a></div>");
+        let classes = item.categories.join(' ');
+        $('#portfolio').append('<div class="project-container ' + classes + '"><a href="' + item.link
+            + '" target="_blank"><div class="project"><div class="img-container"><img src="' + item.image
+            + '" alt="' + item.name + '"></div><h3>' + item.name + '</h3></div></a></div>');
     });
 }
 
 function updateClipboard(newClip) {
     navigator.clipboard.writeText(newClip).then(() => {
         //clipboard successfully set
-        const tooltip = document.getElementById("myTooltip");
-        tooltip.innerHTML = "Copied!";
+        const tooltip = document.getElementById('myTooltip');
+        tooltip.innerHTML = 'Copied!';
     }, () => {
         //clipboard copy failed
-        alert("Failed to copy to clipboard");
+        alert('Failed to copy to clipboard');
     });
 }
 
@@ -47,8 +47,8 @@ function copyEmail() {
 }
 
 function outFunc() {
-    const tooltip = document.getElementById("myTooltip");
-    tooltip.innerHTML = "Copy to clipboard";
+    const tooltip = document.getElementById('myTooltip');
+    tooltip.innerHTML = 'Copy to clipboard';
 }
 
 function setActiveClickLink(target) {
@@ -59,16 +59,16 @@ function setActiveClickLink(target) {
 }
 
 function setActiveScrollLink() {
-    const navLinks = document.getElementsByClassName("nav-link");
+    const navLinks = document.getElementsByClassName('nav-link');
     const hrefs = $.map(navLinks, (elem) => {
-        let result = elem.href.slice(elem.href.indexOf("#"));
+        let result = elem.href.slice(elem.href.indexOf('#'));
         if (result.length === 1) {
-            return result + "intro";
+            return result + 'intro';
         }
         return result;
     });
     const sections = $.grep(hrefs, (elem) => {
-        return elem.indexOf("#") !== -1;
+        return elem.indexOf('#') !== -1;
     });
     $(window).scroll(function() {
         let scroll = $(window).scrollTop() + 1;
@@ -80,15 +80,15 @@ function setActiveScrollLink() {
                 break;
             }
             let nextOffset = $(sections[i + 1]).offset().top;
-            let currentLink = document.getElementsByClassName("active");
+            let currentLink = document.getElementsByClassName('active');
             if (scrollBottom > lastOffset) {
-                currentLink[0].className = currentLink[0].className.replace(" active", "");
-                $(sections[sections.length - 1] + "-link").addClass("active");
+                currentLink[0].className = currentLink[0].className.replace(' active', '');
+                $(sections[sections.length - 1] + '-link').addClass('active');
                 break;
             }
             else if (scroll >= currentOffset && scroll < nextOffset) {
-                currentLink[0].className = currentLink[0].className.replace(" active", "");
-                $(sections[i] + "-link").addClass("active");
+                currentLink[0].className = currentLink[0].className.replace(' active', '');
+                $(sections[i] + '-link').addClass('active');
                 break;
             }
         }
@@ -96,49 +96,49 @@ function setActiveScrollLink() {
 }
 
 function toggleMenu() {
-    $("#nav-bar").toggleClass("responsive");
-    if ($("#nav-bar").hasClass("responsive")) {
-        $("#container").css("width", "50%");
-        $(".line").removeClass("hidden");
-        $(".fas").removeClass("fa-bars");
-        $(".fas").addClass("fa-times");
+    $('#nav-bar').toggleClass('responsive');
+    if ($('#nav-bar').hasClass('responsive')) {
+        $('#container').css('width', '50%');
+        $('.line').removeClass('hidden');
+        $('.fas').removeClass('fa-bars');
+        $('.fas').addClass('fa-times');
     } else {
-        $("#container").css("width", "100%");
-        $(".line").addClass("hidden");
-        $(".fas").removeClass("fa-times");
-        $(".fas").addClass("fa-bars");
+        $('#container').css('width', '100%');
+        $('.line').addClass('hidden');
+        $('.fas').removeClass('fa-times');
+        $('.fas').addClass('fa-bars');
     }
 }
 
 $(window).resize(function () {
     let viewportWidth = $(window).width();
     if (viewportWidth > 786) {
-        $("#nav-bar").removeClass("responsive");
-        $("#container").css("width", "100%");
-        $(".line").addClass("hidden");
-        $(".fas").removeClass("fa-times");
-        $(".fas").addClass("fa-bars");
+        $('#nav-bar').removeClass('responsive');
+        $('#container').css('width', '100%');
+        $('.line').addClass('hidden');
+        $('.fas').removeClass('fa-times');
+        $('.fas').addClass('fa-bars');
     }
 });
 
 function closeMenuOnClick() {
-    $(".nav-link").on('click', function() {
-        $("#nav-bar").removeClass("responsive");
-        $("#container").css("width", "100%");
-        $(".line").addClass("hidden");
-        $(".fas").removeClass("fa-times");
-        $(".fas").addClass("fa-bars");
+    $('.nav-link').on('click', function() {
+        $('#nav-bar').removeClass('responsive');
+        $('#container').css('width', '100%');
+        $('.line').addClass('hidden');
+        $('.fas').removeClass('fa-times');
+        $('.fas').addClass('fa-bars');
     });
 }
 
 function filterProjects() {
-    $(".category").click(function() {
-        let value = $(this).attr("data-filter");
-        if(value === "all") {
-            $(".project-container").show("1000");
+    $('.category').click(function() {
+        let value = $(this).attr('data-filter');
+        if(value === 'all') {
+            $('.project-container').show('1000');
         } else {
-            $(".project-container").not("." + value).hide("3000");
-            $(".project-container").filter("." + value).show("3000");
+            $('.project-container').not('.' + value).hide('3000');
+            $('.project-container').filter('.' + value).show('3000');
         }
     });
 }
@@ -146,9 +146,9 @@ function filterProjects() {
 //Run on page load
 $(function() {
     addProjects(projects);
-    setActiveClickLink("nav-link");
+    setActiveClickLink('nav-link');
     setActiveScrollLink();
-    setActiveClickLink("category");
+    setActiveClickLink('category');
     closeMenuOnClick();
     filterProjects();
 });
