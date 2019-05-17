dbe5f8a68336c3eec0eec92e9e9df98b80e7ae0a
Mark Skinner <markskinner850@gmail.com>
2019-05-16 20:49:37 -0400

    Change menu to appear from top not side

--- js/main.js
+++ js/main.js
@@ -98,12 +98,10 @@ function setActiveScrollLink() {
 function toggleMenu() {
     $('#nav-bar').toggleClass('responsive');
     if ($('#nav-bar').hasClass('responsive')) {
-        $('#container').css('width', '50%');
         $('.line').removeClass('hidden');
         $('.fas').removeClass('fa-bars');
         $('.fas').addClass('fa-times');
     } else {
-        $('#container').css('width', '100%');
         $('.line').addClass('hidden');
         $('.fas').removeClass('fa-times');
         $('.fas').addClass('fa-bars');
@@ -114,7 +112,6 @@ $(window).resize(function () {
     let viewportWidth = $(window).width();
     if (viewportWidth > 786) {
         $('#nav-bar').removeClass('responsive');
-        $('#container').css('width', '100%');
         $('.line').addClass('hidden');
         $('.fas').removeClass('fa-times');
         $('.fas').addClass('fa-bars');
@@ -124,7 +121,6 @@ $(window).resize(function () {
 function closeMenuOnClick() {
     $('.nav-link').on('click', function() {
         $('#nav-bar').removeClass('responsive');
-        $('#container').css('width', '100%');
         $('.line').addClass('hidden');
         $('.fas').removeClass('fa-times');
         $('.fas').addClass('fa-bars');
