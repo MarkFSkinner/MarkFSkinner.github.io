bafc1783ee2146ccad3e8498978c450b363149bb
Mark Skinner <markskinner850@gmail.com>
2019-05-29 21:38:50 -0400

    Add resume link

--- js/main.js
+++ js/main.js
@@ -10,8 +10,8 @@ class Project {
 const projects = [
     new Project('Weather App', 'https://weather-app-39e37.firebaseapp.com/', 'images/weather-app.png', ['apps', 'react']),
     new Project('Drum Machine', 'https://drum-machine-5bfd0.firebaseapp.com/', 'images/drum-machine.png', ['apps', 'react']),
-    new Project('Frogger', 'https://markfskinner.github.io/costanza-frogger-game/', 'images/frogger.png', ['apps']),
-    new Project('Tic Tac Toe', 'https://tic-tac-toe-cd139.firebaseapp.com/', 'images/tic-tac-toe.png', ['apps', 'react']),
+    new Project('Seinfeld Frogger', 'https://markfskinner.github.io/costanza-frogger-game/', 'images/frogger.png', ['apps']),
+    new Project('Unbeatable Tic Tac Toe', 'https://tic-tac-toe-cd139.firebaseapp.com/', 'images/tic-tac-toe.png', ['apps', 'react']),
     new Project('Calculator', 'https://calculator-2aef9.web.app/', 'images/calculator.png', ['apps', 'react']),
     new Project('Pomodoro Clock', 'https://pomodoro-clock-4efc4.firebaseapp.com/', 'images/pomodoro-clock.png', ['apps', 'react']),
     new Project('Markdown Previewer', 'https://markdown-previewer-23554.firebaseapp.com/', 'images/markdown-previewer.png', ['apps', 'react']),
